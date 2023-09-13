1195cb80ee450987faa7e187df19821c4d3a32a8
Fernando Esdras da Silva <contatoesdrasoficial@gmail.com>
2023-09-13 20:59:43 -0300

    first commit

---
+++ src/config/dbConfig.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+const uri =
+  `mongodb+srv://${dbUser}:${dbPassword}@cluster1.uy4rhr2.mongodb.net/?retryWrites=true&w=majority`;
+
+const connection = mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+module.exports = connection;
