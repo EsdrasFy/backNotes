bd27018836f58bd2966616d4986208ae2eff2958
Fernando Esdras da Silva <contatoesdrasoficial@gmail.com>
2023-09-13 22:01:13 -0300

    update render

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -4,8 +4,7 @@ require("dotenv").config();
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASS
 
-const uri =
-  `mongodb+srv://${dbUser}:${dbPassword}@cluster1.uy4rhr2.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://esdrasTodo:13579@cluster1.uy4rhr2.mongodb.net/?retryWrites=true&w=majority`;
 
 const connection = mongoose.connect(uri, {
   useNewUrlParser: true,
